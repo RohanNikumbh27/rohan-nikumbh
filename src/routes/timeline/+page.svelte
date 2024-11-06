793fa0c0b4ee44bd624a1018f8b7fc6032c8a237
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2024-11-06 22:47:19 +0530

    timeline improved

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -1,80 +1,29 @@
 <script>
-  // export let data;
-  // console.log("data", data.data)
+  import ProgressComp from "./ProgressComp.svelte";
+
 </script>
-<!-- <div class=" container h-svh flex items-center justify-center font-serif font-bold flex-col">
-  <h1 class="font-bold text-3xl">Timeline Section</h1>
-  <p>Under Construction</p>
-</div> -->
-<!-- <div class=" container h-svh flex items-center justify-center font-serif font-bold flex-col">
-<div class="h-2 w-2 bg-black rounded-full"></div>
-</div> -->
 
 
 
 
 <div class=" p-4">
   <div class="flex flex-col grid-cols-9 p-2 mx-auto md:grid">
-      <div class="flex md:contents flex-row-reverse">
-          <div
-              class="relative p-4 my-6 text-gray-300 bg-zinc-900 rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
-              <h3 class="text-lg font-semibold lg:text-xl">New Event 1</h3>
-              <p class="mt-2 leading-6">Description of the first event.</p>
-              <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 1</span>
-          </div>
-          <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
-              <div class="flex items-center justify-center w-6 h-full">
-                  <div class="w-1 h-full bg-indigo-300 rounded-t-full bg-gradient-to-b from-indigo-400 to-indigo-300">
-                  </div>
-              </div>
-              <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-          </div>
-      </div>
-
-      <div class="flex md:contents">
-          <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
-              <div class="flex items-center justify-center w-6 h-full">
-                  <div class="w-1 h-full bg-indigo-300"></div>
-              </div>
-              <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-          </div>
-          <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
-              <h3 class="text-lg font-semibold lg:text-xl">New Event 2</h3>
-              <p class="mt-2 leading-6">Description of the second event.</p>
-              <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 2</span>
-          </div>
-      </div>
-
-      <div class="flex md:contents flex-row-reverse">
-          <div
-              class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-1 col-end-5 mr-auto md:mr-0 md:ml-auto">
-              <h3 class="text-lg font-semibold lg:text-xl">New Event 3</h3>
-              <p class="mt-2 leading-6">Description of the first event.</p>
-              <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 3</span>
-          </div>
-          <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
-              <div class="flex items-center justify-center w-6 h-full">
-                  <div class="w-1 h-full bg-indigo-300 rounded-t-full bg-gradient-to-b from-indigo-400 to-indigo-300">
-                  </div>
-              </div>
-              <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-          </div>
-      </div>
-
-      <div class="flex md:contents">
-          <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto">
-              <div class="flex items-center justify-center w-6 h-full">
-                  <div class="w-1 h-full bg-indigo-300"></div>
-              </div>
-              <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-          </div>
-          <div class="relative p-4 my-6 text-gray-800 bg-white rounded-xl col-start-6 col-end-10 mr-auto">
-              <h3 class="text-lg font-semibold lg:text-xl">New Event 4</h3>
-              <p class="mt-2 leading-6">Description of the second event.</p>
-              <span class="absolute text-sm text-indigo-100/75 -top-5 left-2 whitespace-nowrap">Date 4</span>
-          </div>
-      </div>
-
-  </div>
+    <ProgressComp pos="left" title="Software Engineer Intern" desc="@Timechain labs (4 months)" date="June 24 - Oct 2024"/>
+    <ProgressComp pos="right" title="Software Engineer Intern" desc="@Timechain labs (4 months)" date="June 24 - Oct 2024"/>
+  </div> 
 </div>
 
+
+
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    gap: 2rem;
+    padding: 2rem;
+    /* background: radial-gradient(circle, #222, #111); */
+    height: 100vh;
+    align-items: start;
+  }
+</style>
