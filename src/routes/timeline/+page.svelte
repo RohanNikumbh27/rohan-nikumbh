2756980a8c420619278e2d82dbef08a3d4ce8649
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2024-11-08 10:05:20 +0530

    skills section

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -8,8 +8,11 @@
 
 <div class=" p-4">
   <div class="flex flex-col grid-cols-9 p-2 mx-auto md:grid">
-    <ProgressComp pos="left" title="Software Engineer Intern" desc="@Timechain labs (4 months)" date="June 24 - Oct 2024"/>
-    <ProgressComp pos="right" title="Software Engineer Intern" desc="@Timechain labs (4 months)" date="June 24 - Oct 2024"/>
+    <ProgressComp pos="left" title="Software Engineer Intern" desc="@Timechain labs" date="June 24 - Oct 2024"/>
+    <ProgressComp pos="right" title="Web Developer Intern" desc="CodSoft" date="1 Aug 2023 - 31 Aug 2023"/> 
+    <ProgressComp pos="left" title="B.Tech in Information Technology" desc="VIIT, Pune" date="June 22 - June 25"/>
+    <ProgressComp pos="right" title="Diploma Engineer Trainee" desc="ARaymond India Pvt. Ltd." date="Aug 2022 - Nov 2022"/> 
+    <ProgressComp pos="left" title="Diploma in E&TC" desc="Government Polytechnic Nashik" date="Aug 2019 - July 2022"/> 
   </div> 
 </div>
 
--- src/routes/timeline/ProgressComp.svelte
+++ src/routes/timeline/ProgressComp.svelte
@@ -25,16 +25,16 @@
                 <div class="w-1 h-full bg-zinc-300 rounded-t-full ">
                 </div>
             </div>
-            {#if hovered}
+            <!-- {#if hovered}
               <div class="relative flex items-center h-12 z-10 -top-[64%] mb-10 rotate-180 transition-all">
                 <div class="w-6 h-6 bg-zinc-900 border-t-4 border-s-4 border-b-4 border-zinc-900 rounded-full"></div>
                 <div class="flex-1 h-4 bg-zinc-900 border-t-4 border-b-4 border-zinc-900 rounded-l-full relative">
                   <div class="absolute w-4 h-4  top-[51.9%] -right-[2px]  translate-y-[-50%] rotate-[-52deg] skew-y-[14deg]	transform: skewY(12deg); bg-zinc-900"></div>
                 </div>
               </div>
-            {:else}
-              <div class=" transition-all absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-            {/if}
+            {:else} -->
+              <div class=" transition-all absolute w-6 h-6 -mt-3 bg-white border-4 border-zinc-800 rounded-full top-1/2"></div>
+            <!-- {/if} -->
               </div>
     </div>
 {/if}
@@ -48,20 +48,20 @@
     >
         <div class="relative col-start-5 col-end-6 mr-7 md:mx-auto ">
             <div class="flex items-center justify-center w-6 h-full">
-                <div class="w-1 h-full bg-indigo-300"></div>
+                <div class="w-1 h-full bg-zinc-300"></div>
             </div>
-            {#if hovered}
-              <div class="relative flex items-center h-12 z-10 -top-[64%] mb-10 ">
+            <!-- {#if hovered}
+              <div class="relative flex items-center h-12 z-10 -top-[64%] mb-10">
                 <div class="w-6 h-6 bg-zinc-900 border-t-4 border-s-4 border-b-4 border-zinc-900 rounded-full"></div>
                 <div class="flex-1 h-4 bg-zinc-900 border-t-4 border-b-4 border-zinc-900 rounded-l-full relative">
                   <div class="absolute w-4 h-4  top-[51.9%] -right-[2px]  translate-y-[-50%] rotate-[-52deg] skew-y-[14deg]	transform: skewY(12deg); bg-zinc-900"></div>
                 </div>
               </div>
-            {:else}
-              <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-indigo-400 rounded-full top-1/2"></div>
-            {/if}
+            {:else} -->
+              <div class="absolute w-6 h-6 -mt-3 bg-white border-4 border-zinc-800 rounded-full top-1/2"></div>
+            <!-- {/if} -->
         </div>
-        <div class="relative p-4 my-6 text-zinc-300 bg-rad rounded-xl col-start-6 col-end-10 mr-auto">
+        <div class="relative p-7 my-6 text-zinc-300 bg-rad rounded-xl col-start-6 col-end-10 mr-auto">
             <span class=" text-sm text-zinc-300  whitespace-nowrap">{date}</span>
             <h3 class="text-lg font-semibold lg:text-xl">{title}</h3>
             <p class="mt-2 leading-6 text-zinc-400">{desc}</p>
