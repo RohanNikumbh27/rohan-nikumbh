08ca7f735a8139a3a397fef7750790a69479e2a2
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2024-12-05 12:25:23 +0530

    projects under development

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -1,9 +1,12 @@
 <script>
+    export let data;
     // import ProjectCard from '$lib/components/Cards/ProjectCard.svelte';
     import { onMount } from 'svelte';
     import ImageZoomCard from "$lib/components/Cards/ImageZoomCard.svelte";
-  import ProjectDetails from '../../lib/components/Cards/ProjectDetails.svelte';
+    import ProjectDetails from '../../lib/components/Cards/ProjectDetails.svelte';
 
+
+  
     let projects = [];
     let showDesc = true;
     const handleShowDesc = () => {
@@ -18,17 +21,20 @@
 </script>
 
 <div class="flex items-center justify-center md:mt-10">
-    <h1 class="mx-auto font-bold text-2xl text-slate-700">My Projects</h1>
+    <h1 class="mx-auto font-semibold text-2xl text-slate-700">My Projects</h1>
 </div>
 
 <div class="h-5"></div>
 
 <div class="container grid gap-10 md:grid-cols-3 sm:grid-cols-1">
-    {#each projects as project}
-        <!-- <ProjectCard {project} /> -->
-            <ImageZoomCard {project} on:click{handleShowDesc}/>
-            <!-- {#if showDesc}
-            {/if} -->
-    {/each}
+    <!-- {#each projects as project}
+            <a href="/personal-expense-tracker">
+                <ImageZoomCard {project}/>
+            </a>
+    {/each} -->
 
+    
+</div>
+<div class="flex items-center justify-center md:m-10">
+    <h1 class="mx-auto font-semibold text-2xl text-slate-700">Under Development</h1>
 </div>
--- src/routes/work/[projectId]/+page.svelte
+++ src/routes/work/[projectId]/+page.svelte
@@ -1,11 +1,15 @@
 <script>
     import { page } from '$app/stores';
+    export let data;
   import ProjectDetails from '../../../lib/components/Cards/ProjectDetails.svelte';
     const projectId = $page.params.projectId;
 </script>
 <div>
     {projectId}
     
-    <ProjectDetails/>
+    <!-- <ProjectDetails/> -->
+
+    <h1>{data.post.title}</h1>
+<div>{@html data.post.content}</div>
 
 </div>
