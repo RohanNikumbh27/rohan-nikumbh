cfefca07e3dcef3bb74113608035e7c839309b80
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2025-06-22 13:52:04 +0530

    github stats color fixed

--- src/lib/components/Home/GithubStats.svelte
+++ src/lib/components/Home/GithubStats.svelte
@@ -12,11 +12,11 @@
 
 <div class="flex items-center justify-center pt-10 md:pt-0 ">
     <Github class="w-6 h-6 text-[#204dff] mr-3" />
-    <span class="text-zinc-900 text-3xl dark:text-zinc-100">Github Stats <span class="text-sm text-zinc-500 ">(31/01/2025)</span></span>
+    <span class=" text-zinc-900 text-3xl dark:text-zinc-100">Github Stats <span class="text-sm text-zinc-500 ">(31/01/2025)</span></span>
   </div>
   
   <section class="m-[15px]">
-    <div  class="md:mb-10  bg-black/70 text-white px-6 py-8 max-w-sm sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl mx-auto my-2 rounded-xl">
+    <div  class="md:mb-10  bg-black text-white px-6 py-8 max-w-sm sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl mx-auto my-2 rounded-xl">
       <div class="space-y-4">
         <div class="flex gap-5 flex-col sm:flex-col md:flex-row justify-between">
           <div class="flex items-center w-full xl:px-5 "> 
