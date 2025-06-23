57ffe7110a24b7bf791f46e8dcf4232228b863eb
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2025-06-23 23:21:14 +0530

    perspective added to other cards with hover scale

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,6 +1,7 @@
 <script>
   import { fly } from 'svelte/transition';
   let hover = false;
+  import {perspectiveTilt} from '$lib/utils.js';
 
   export let skills = [
     {
@@ -126,7 +127,7 @@
         <h1 class="pt-5 px-5 text-3xl bg-gradient-to-br from-primary to-primary bg-clip-text text-transparent  transition-all">{section.category}</h1>
         <div class="flex justify-center items-center flex-wrap py-5 px-5">
             {#each section.skills as skill, i}
-              <div class="skill-card flex items-center gap-3 py-2 px-4 md:px-5 m-1 md:m-2 w-full md:w-auto cursor-pointer bg-zinc-950/80 dark:bg-white/5 rounded-2xl text-white shadow-md hover:shadow-xl transition-all duration-300 hover:scale-105 hover:bg-primary group-hover:bg-primary">
+              <div use:perspectiveTilt={{scale:1.1}} class="skill-card flex items-center gap-3 py-2 px-4 md:px-5 m-1 md:m-2 w-full md:w-auto cursor-pointer bg-zinc-950/80 dark:bg-white/5 rounded-2xl text-white shadow-md hover:shadow-xl transition-all duration-300 hover:scale-105 hover:bg-primary group-hover:bg-primary">
                 <span class="font-medium text-base md:text-lg">{skill.name}</span>
                </div>
             {/each}
