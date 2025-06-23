ae7c2e0a977da589beff6b7cbb27f39b09963e40
Rohan Nikumbh <rohannikumbh2793@gmail.com>
2025-06-23 21:33:36 +0530

    perspective reusable action, hero image perspective, format

--- src/lib/components/HeroSection.svelte
+++ src/lib/components/HeroSection.svelte
@@ -1,8 +1,9 @@
 <script>
   import Button from "$lib/components/form/Button.svelte";
   import Loader from "$lib/components/Loader.svelte";
+  import HeroImage from "$lib/components/Home/HeroImage.svelte";
   import {onMount} from "svelte";
-  import {blur,fade} from "svelte/transition";
+
 
   let scrollPosition = 0;
   let currentIndex = 0;
@@ -46,14 +47,7 @@
       {#if showLoader}
         <Loader className="h-[500px] w-[330px] md:w-[370px] min-w-[340px] transition-all rounded-2xl md:mt-6" />
       {:else}
-      <img
-        transition={{ duration:1000, amount: 20 }}
-        src='/Rohan_Nikumbh.jpg'
-        style="will-change: opacity"
-        alt="Img"
-        loading="lazy"
-        class="rounded-2xl !h-[500px] !w-[330px] !md:w-[370px] min-w-[340px] object-cover border-b-2  border-primary md:mt-8"
-      />
+        <HeroImage/>
       {/if}
     </div>
   </div>
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -13,3 +13,35 @@ export function clickOutside(node) {
 		}
 	};
 }
+
+export function perspectiveTilt(node, { multiplier = 10 } = {}) {
+  let rotationX = 0;
+  let rotationY = 0;
+
+  function handleMouseMove(e) {
+    const rect = node.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    rotationY = ((x - centerX) / centerX) * multiplier;
+    rotationX = -((y - centerY) / centerY) * multiplier;
+    node.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+  }
+
+  function handleMouseLeave() {
+    rotationX = 0;
+    rotationY = 0;
+    node.style.transform = `rotateX(0deg) rotateY(0deg)`;
+  }
+
+  node.addEventListener("mousemove", handleMouseMove);
+  node.addEventListener("mouseleave", handleMouseLeave);
+
+  return {
+    destroy() {
+      node.removeEventListener("mousemove", handleMouseMove);
+      node.removeEventListener("mouseleave", handleMouseLeave);
+    },
+  };
+}
